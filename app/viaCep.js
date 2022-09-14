ab41fca04e5e9f466caf096ebbaae1dddabeb5a4
João Ougano <pedroougano@gmail.com>
2022-09-14 16:22:52 -0300

    sugerindo coisas bobas

--- app/viaCep.js
+++ app/viaCep.js
@@ -1,24 +1,20 @@
-async function getCep(cep) {
+const getCep = async (cep) => {
+  const mensagemErro = document.getElementById("erro");
+  mensagemErro.innerHTML = "";
 
-    var mensagemErro = document.getElementById('erro')
-    mensagemErro.innerHTML = ''
+  try {
+    //Seria legal depois colocar a url da API em uma variavel de ambiente pra ela nao ficar exposta
+    const viaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
 
-    try {
+    const viaCepJSON = await viaCep.json();
 
-        const viaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-
-        const viaCepJSON = await viaCep.json()
-
-        if (viaCepJSON.erro) {
-            throw Error('CEP não existe!')
-        } 
-
-        preencheForms(viaCepJSON)
-
-        return viaCepJSON
-    } catch (erro) {
-        mensagemErro.innerHTML = `<p> CEP inválido. Tente novamente! </p>`
+    if (viaCepJSON.erro) {
+      throw Error("CEP não existe!");
     }
 
-}
-
+    preencheForms(viaCepJSON);
+    return viaCepJSON;
+  } catch (erro) {
+    mensagemErro.innerHTML = `<p> CEP inválido. Tente novamente! </p>`;
+  }
+};
